5b8089ed2729642f9b5033d7aae5f29280bea106
Shivansh9848 <104058319+shivansh9848@users.noreply.github.com>
2024-06-24 22:01:18 +0530

    a

--- Graph Algorithms/UnionFind.cpp
+++ Graph Algorithms/UnionFind.cpp
@@ -1,30 +1,27 @@
+
 class DSU
 {
 public:
     vector<int> parent;
     vector<int> size;
+
     DSU(int n)
     {
-        parent.resize(n+1);
-        size.resize(n+1, 1);
-        for (int i = 0; i <= n; i++)
+        parent.resize(n);
+        size.resize(n, 1);
+        for (int i = 0; i < n; i++)
         {
             parent[i] = i;
             size[i] = 1;
         }
     }
-    void make_set(int v)
-    {
-        parent[v] = v;
-        size[v] = 1;
-    }
+
     int find_set(int v)
     {
         if (v == parent[v])
             return v;
         return parent[v] = find_set(parent[v]);
     }
-
 
     void union_sets(int a, int b)
     {
